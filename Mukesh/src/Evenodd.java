6de0fda3bd0ca47e14cd63349bfda879a94e77b3
Admin <Admin@192.168.0.2>
2019-06-25 19:57:38 +0530

    added class Evenodd

--- Mukesh/src/Evenodd.java
+++ Mukesh/src/Evenodd.java
@@ -1,4 +1,12 @@
 
 public class Evenodd {
-
+		public static void main(String[]args) {
+			int i = 15;
+			if(i%2==1) {
+				System.out.println("odd");
+			if(i%2==0) {
+					System.out.println("even");
+				}
+			}
+		}
 }
