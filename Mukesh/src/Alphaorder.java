3df6b5b5d4c3c55264ff312169735475b02222ca
Admin <Admin@DESKTOP-49FEGJC>
2019-06-25 11:28:39 +0530

    added class Alphaorder.java

---
+++ Mukesh/src/Alphaorder.java
@@ -0,0 +1,14 @@
+
+public class Alphaorder {
+	public static void main(String[]args) {
+		char one='f';
+		char two='c';
+		if(one< two) {
+			System.out.println(one+","+two);
+		}
+		    else {
+				System.out.println(two+","+one);
+		}
+	}
+
+}
